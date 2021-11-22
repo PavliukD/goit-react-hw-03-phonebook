ba36b82999164f2147fa890981a44056f08071db
Dmitriy <albatrozzz1@gmail.com>
2021-11-22 20:54:58 +0200

    Выполнено задание согласно условия

--- src/App.js
+++ src/App.js
@@ -7,13 +7,20 @@ import ContactList from "./components/ContactList/ContactList";
 
 class App extends Component{
   state = {
-    contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
+    contacts: [],
     filter: ''
+  }
+
+  componentDidUpdate(){
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+  }
+
+  componentDidMount(){
+    const contacts = JSON.parse(localStorage.getItem('contacts'))
+    if (contacts){
+    this.setState({contacts})
+    }
+
   }
 
   inputChange = (event) => {
@@ -73,3 +80,9 @@ class App extends Component{
 }
 
 export default App;
+
+
+// {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+// {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+// {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+// {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
